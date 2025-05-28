4f4b18260de13f13fe8c5255cc575d9926bb74fd
kucziakczeremeta <140640702+kucziakczeremeta@users.noreply.github.com>
2025-05-28 16:05:29 -0300

    jj

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const botoes = document.querySelectorAll(".botoes");
+const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 for (let i=0; i < botoes.length; i++){
     botoes [i].onclick = function(){
@@ -10,7 +10,7 @@ for (let i=0; i < botoes.length; i++){
         textos [i].classList.add ("ativo");
     }
 }
-const contadores = document.querySelectorAll("contador");
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2035-01-01T00:00:00");
 const tempoobjetivo2 = new Date ("2031-03-21T19:00:00");
 const tempoObjetivo3 = new Date ("2031-04-26T00:00:00");
